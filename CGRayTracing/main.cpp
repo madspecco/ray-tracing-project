bf74bebbae0609f32cd2096b27e3e3df1e62ba8a
madspecco <bogdan.bolos30@gmail.com>
2024-04-01 20:35:24 +0300

    introduced interval class

--- CGRayTracing/main.cpp
+++ CGRayTracing/main.cpp
@@ -10,26 +10,9 @@
 #include "sphere.h"
 
 
-//double hit_sphere(const point3& center, double radius, const ray& r) {
-//	vec3 oc = r.origin() - center;
-//	auto a = r.direction().length_squared();
-//	auto half_b = dot(oc, r.direction());
-//	auto c = oc.length_squared() - radius * radius;
-//	auto discriminant = half_b * half_b - a * c;
-//
-//	if (discriminant < 0) {
-//		return -1.0;
-//	}
-//
-//	else {
-//		return (-half_b - sqrt(discriminant)) / a;
-//	}
-//}
-
-
 color ray_color(const ray& r, const hittable& world) {
 	hit_record rec;
-	if (world.hit(r, 0, infinity, rec)) {
+	if (world.hit(r, interval(0, infinity), rec)) {
 		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
--- CGRayTracing/utilities.h
+++ CGRayTracing/utilities.h
@@ -5,7 +5,14 @@
 #include <limits>
 #include <memory>
 
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+
 // Common Headers
+#include "interval.h"
 #include "ray.h"
 #include "vec3.h"
 
@@ -13,10 +20,6 @@
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
-
-// Constants
-const double infinity = std::numeric_limits<double>::infinity();
-const double pi = 3.1415926535897932385;
 
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
