f0dbeb8cf105be13effe8a45c139389f160a14f5
madspecco <bogdan.bolos30@gmail.com>
2024-04-01 20:25:07 +0300

    added "ground" to rendering
    - provides a picture that is a visualization of where the spheres are located along with their surface normal

--- CGRayTracing/main.cpp
+++ CGRayTracing/main.cpp
@@ -1,39 +1,43 @@
 #include <iostream>
 #include <fstream>
 
+#include "utilities.h"
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	auto a = r.direction().length_squared();
-	auto half_b = dot(oc, r.direction());
-	auto c = oc.length_squared() - radius * radius;
-	auto discriminant = half_b * half_b - a * c;
+//double hit_sphere(const point3& center, double radius, const ray& r) {
+//	vec3 oc = r.origin() - center;
+//	auto a = r.direction().length_squared();
+//	auto half_b = dot(oc, r.direction());
+//	auto c = oc.length_squared() - radius * radius;
+//	auto discriminant = half_b * half_b - a * c;
+//
+//	if (discriminant < 0) {
+//		return -1.0;
+//	}
+//
+//	else {
+//		return (-half_b - sqrt(discriminant)) / a;
+//	}
+//}
 
-	if (discriminant < 0) {
-		return -1.0;
-	}
 
-	else {
-		return (-half_b - sqrt(discriminant)) / a;
-	}
-}
-
-
-color ray_color(const ray& r) {
-	auto t = hit_sphere(point3(0, 0, -1), -0.5, r);
-	if (t > 0.0) {
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)) {
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
+
 
 
 int main()
@@ -47,8 +51,12 @@ int main()
 	int image_height = static_cast<int>(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
 
-	// Camera
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
+	// Camera
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
 	auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
@@ -89,7 +97,7 @@ int main()
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(outputFile, pixel_color);
 		}
 	}
