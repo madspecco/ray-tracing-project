85cadb24810e8c18101678e5c137e58eabf330b9
madspecco <bogdan.bolos30@gmail.com>
2024-04-01 12:32:39 +0300

    write to file

--- CGRayTracing/main.cpp
+++ CGRayTracing/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 
 int main()
@@ -7,8 +8,17 @@ int main()
 	int image_width = 256;
 	int image_height = 256;
 
+	// open file for writing
+	std::ofstream outputFile("output.ppm");
+
+	// check if file opened successfully
+	if (!outputFile.is_open()) {
+		std::cerr << "Error: Could not open file.\n";
+		return 1;
+	}
+
 	// rendering
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	outputFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = 0; j < image_height; j++) {
 		for (int i = 0; i < image_height; i++) {
@@ -20,7 +30,7 @@ int main()
 			int ig = static_cast<int>(255.99 * g);
 			int ib = static_cast<int>(255.99 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			outputFile << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
 }
