9f39d0f5abefd7c5e589808e6ba71f497814a1f5
madspecco <bogdan.bolos30@gmail.com>
2024-04-03 12:34:50 +0300

    added field of view to camera

--- CGRayTracing/camera.h
+++ CGRayTracing/camera.h
@@ -18,6 +18,9 @@ public:
     int    samples_per_pixel = 10;      // Count of random samples for each pixel
     int    max_depth = 10;              // Maximum number of ray bounces into scene
 
+    double vfov = 90;                   // Vertical view angle (field of view)
+
+
     void render(const hittable& world) {
         initialize();
 
@@ -62,8 +65,11 @@ private:
 
         // Determine viewport dimensions.
         auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
         auto viewport_u = vec3(viewport_width, 0, 0);
